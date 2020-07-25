bd200e5b7b681843f7ea156911fef707c285c38b
sinhlt58 <sinhlt58@gmail.com>
2020-07-25 08:02:24 +0700

    init

---
+++ src/main/java/ghtk/rnd/process_kafka/Main.java
@@ -0,0 +1,10 @@
+package ghtk.rnd.process_kafka;
+
+public class Main {
+
+    
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("HEHEHE");
+    }
+}
