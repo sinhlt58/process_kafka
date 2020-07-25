5975d6457f656ce4726a42cac360834b39ff89b2
sinhlt58 <sinhlt58@gmail.com>
2020-07-25 08:32:15 +0700

    update

--- src/main/java/ghtk/rnd/process_kafka/Main.java
+++ src/main/java/ghtk/rnd/process_kafka/Main.java
@@ -1,10 +1,28 @@
 package ghtk.rnd.process_kafka;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
 
-    
+    public static ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
 
     public static void main(final String[] args) throws Exception {
-        System.out.println("HEHEHE");
+        for (int i=0; i<5; i++){
+            executor.submit(() -> {
+//                throw new Exception("Fake exception");
+                try{
+                    Thread.sleep(1000);
+                    System.out.println("sss");
+                    throw new Exception("Fake exception");
+                } catch (Exception e){
+                    System.out.print(("In here"));
+                    throw new Exception("Fake exception");
+                }
+            });
+        }
+        Thread.sleep(2000);
+        System.out.println(executor.getActiveCount());
     }
 }
